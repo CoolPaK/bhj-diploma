5b23e2db3b763338cbb24cf2966ba959165846df
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-11-26 02:36:06 +0300

    add file

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -12,7 +12,12 @@ class Modal {
    * необходимо выкинуть ошибку.
    * */
   constructor(element){
+    if (!element) {
+      throw new Error("Element does not exist!");
+    }
 
+    this.element = element;
+    this.registerEvents();
   }
 
   /**
@@ -21,7 +26,11 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
-
+    for (let btn of this.element.querySelectorAll('[data-dismiss="modal"]')) {
+      btn.addEventListener("click", (e) => {
+        this.onClose(e);
+      });
+    }
   }
 
   /**
@@ -29,19 +38,20 @@ class Modal {
    * Закрывает текущее окно (Modal.close())
    * */
   onClose(e) {
-
-  }
+    e.preventDefault();
+    this.close();
+}
   /**
    * Открывает окно: устанавливает CSS-свойство display
    * со значением «block»
    * */
   open() {
-
+    this.element.style.display = "block";
   }
   /**
    * Закрывает окно: удаляет CSS-свойство display
    * */
   close(){
-
+    this.element.style.display = "none";
   }
 }
